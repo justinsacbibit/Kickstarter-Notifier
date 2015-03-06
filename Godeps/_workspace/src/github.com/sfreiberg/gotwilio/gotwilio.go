c44240a9b16360a43b07d7de55c756efc0aba617
Justin Sacbibit <jsacbibit@gmail.com>
2015-03-05 22:12:21 -0500

    Initial commit

---
+++ Godeps/_workspace/src/github.com/sfreiberg/gotwilio/gotwilio.go
@@ -0,0 +1,47 @@
+// Package gotwilio is a library for interacting with http://www.twilio.com/ API.
+package gotwilio
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// Twilio stores basic information important for connecting to the
+// twilio.com REST api such as AccountSid and AuthToken.
+type Twilio struct {
+	AccountSid string
+	AuthToken  string
+	BaseUrl    string
+	HTTPClient *http.Client
+}
+
+// Exception is a representation of a twilio exception.
+type Exception struct {
+	Status   int    `json:"status"`    // HTTP specific error code
+	Message  string `json:"message"`   // HTTP error message
+	Code     int    `json:"code"`      // Twilio specific error code
+	MoreInfo string `json:"more_info"` // Additional info from Twilio
+}
+
+// Create a new Twilio struct.
+func NewTwilioClient(accountSid, authToken string) *Twilio {
+	twilioUrl := "https://api.twilio.com/2010-04-01" // Should this be moved into a constant?
+	return &Twilio{accountSid, authToken, twilioUrl, http.DefaultClient}
+}
+
+func (twilio *Twilio) post(formValues url.Values, twilioUrl string) (*http.Response, error) {
+	req, err := http.NewRequest("POST", twilioUrl, strings.NewReader(formValues.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(twilio.AccountSid, twilio.AuthToken)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := twilio.HTTPClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	return client.Do(req)
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "regexp"
+    "strings"
+    "strconv"
+    "os"
+    "time"
+    "net/http"
+
+    "github.com/PuerkitoBio/goquery"
+    "github.com/sfreiberg/gotwilio"
+)
+
+func Scrape(rgx *regexp.Regexp) uint64 {
+    doc, err := goquery.NewDocument("https://www.kickstarter.com/projects/597507018/pebble-time-awesome-smartwatch-no-compromises")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var remaining uint64
+    doc.Find(".limited-number").Each(func(i int, s *goquery.Selection) {
+        timeSteel := 3
+        if i != timeSteel {
+            return
+        }
+        span := s.Text()
+        raw := rgx.FindString(span)
+        remaining, _ = strconv.ParseUint(strings.Trim(raw, "( "), 10, 32)
+    })
+
+    return remaining
+}
+
+func doEvery(d time.Duration, f func()) {
+    for {
+        time.Sleep(d)
+        f()
+    }
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    w.Write([]byte("<h1>Pebble availability</h1>"))
+}
+
+func main() {
+    accountSid := os.Getenv("twilioSid")
+    authToken := os.Getenv("twilioToken")
+    twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+    from := os.Getenv("fromNum")
+    to := os.Getenv("toNum")
+
+    rgx := regexp.MustCompile("\\((.*?) ")
+
+    go doEvery(60*time.Second, func() {
+        r := Scrape(rgx)
+        message := fmt.Sprintf("%d Pebble Time Steels of %d are remaining.", r, 20000)
+        fmt.Printf("Sending message: %s\n", message)
+        twilio.SendSMS(from, to, message, "", "")
+    })
+
+    http.HandleFunc("/", handler)
+    err := http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+    if err != nil {
+        panic(err)
+    }
+}
