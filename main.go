18d336bc34a632378e6e4859a96bf2aa57db88e8
Justin Sacbibit <jsacbibit@gmail.com>
2015-03-07 23:52:28 -0500

    WIP on making app more modular

--- main.go
+++ main.go
@@ -9,56 +9,115 @@ import (
     "strings"
 
     "github.com/PuerkitoBio/goquery"
+    _ "github.com/garyburd/redigo/redis"
     "github.com/sfreiberg/gotwilio"
 )
 
-var rgx *regexp.Regexp
+type RewardsParser struct {
+    /**
+     * Parses out remaining number of rewards
+     * Example input: Limited (5093 left of 26000)
+     * Example match: (5093
+     */
+    remainingRegex *regexp.Regexp
 
-func Scrape(proj string, idx uint) int64 {
-    doc, err := goquery.NewDocument("https://www.kickstarter.com/projects/" + proj)
-    if err != nil {
-        fmt.Println("Error getting doc")
-        fmt.Println(err)
-        return -1
+    /**
+     * Parses out total number of rewards
+     * Example input: Limited (5093 left of 26000)
+     * Example match:  26000)
+     */
+    totalRegex *regexp.Regexp
+
+    /**
+     * Determines if remaining number of rewards is 0
+     */
+    soldOutRegex *regexp.Regexp
+}
+
+/**
+ */
+func (p *RewardsParser) compileRegexsIfNecessary() {
+    if p.remainingRegex == nil {
+        p.remainingRegex = regexp.MustCompile("\\((.*?) ")
+        p.totalRegex = regexp.MustCompile("f (.*?)\\)")
+        p.soldOutRegex = regexp.MustCompile("All gone!")
+    }
+}
+
+/**
+ * Looks for remaining and total number of rewards
+ * @param  string  s   Input string to be searched
+ * @return integer r   Remaining rewards
+ * @return integer t   Total rewards
+ * @return error   err Error if failure
+ */
+func (p *RewardsParser) parseRewardAmounts(s string) (int64, int64, error) {
+    p.compileRegexsIfNecessary()
+
+    if p.soldOutRegex.FindString(s) != "" {
+        return 0, 0, nil
     }
 
-    var remaining int64 = -1
-    parseErr := false
+    ur := p.remainingRegex.FindString(s)
+    if ur == "" {
+        return 0, 0, fmt.Errorf("error parsing string for reward amounts: %s", s)
+    }
+    ut := p.totalRegex.FindString(s)
+    if ut == "" {
+        return 0, 0, fmt.Errorf("error parsing string for total amount: %s", s)
+    }
+
+    var r, t int64
+    var err error
+
+    r, err = strconv.ParseInt(strings.Trim(ur, "( "), 10, 32)
+    if err != nil {
+        return 0, 0, err
+    }
+
+    t, err = strconv.ParseInt(strings.Trim(ut, "f )"), 10, 32)
+    if err != nil {
+        return 0, 0, err
+    }
+
+    return r, t, err
+}
+
+var rewardsParser *RewardsParser
+
+/*
+ * Gets remaining and total amounts for a Kickstarter project reward
+ * @param  string proj      Kickstarter project URL suffix like: "597507018/pebble-time-awesome-smartwatch-no-compromises"
+ * @param  uint   idx       Zero-based index of reward
+ * @return int64  remaining Amount remaining of the reward
+ * @return int64  total     Total amount of reward (will be 0 if amount remaining is 0)
+ * @return error  err       Error if one has occurred
+ */
+func scrape(proj string, idx uint) (int64, int64, error) {
+    // TODO:
+    // Make this method more testable.
+    // It's tightly coupled to goquery at the moment.
+    doc, err := goquery.NewDocument("https://www.kickstarter.com/projects/" + proj)
+    if err != nil {
+        return 0, 0, err
+    }
+
+    var remaining, total int64
     doc.Find(".NS-projects-reward").EachWithBreak(func(i int, s *goquery.Selection) bool {
         if uint(i) != idx {
             return true
         }
 
-        span := s.Find(".limited-number").Text() // Limited (5093 left of 26000)
-        raw := rgx.FindString(span)              // (5093  (trailing space)
-
-        if raw == "" {
-            // check if none remaining
-            span = s.Find(".sold-out").Text()
-            if span == "" {
-                fmt.Println("Failed to parse number of remaining")
-                fmt.Println(s.Text())
-                parseErr = true
-            } else {
-                remaining = 0
-            }
-            return false
-        }
-
-        remaining, err = strconv.ParseInt(strings.Trim(raw, "( "), 10, 32) // 5093
-        if err != nil {
-            fmt.Println("Error parsing int")
-            fmt.Println(err)
-            parseErr = true
-        }
+        span := s.Find(".limited-number").Text()
+        remaining, total, err = rewardsParser.parseRewardAmounts(span)
         return false
     })
 
-    if parseErr {
-        return -1
+    if err != nil {
+        return 0, 0, err
     }
 
-    return remaining
+    return int64(remaining), int64(total), nil
 }
 
 // Given the last sent amount and the current remaining amount, determines if a new text should be sent
@@ -81,7 +140,7 @@ func sendMessage(lastSentAmount int64, currentAmount int64) bool {
     return changed
 }
 
-var accountSid, authToken, from, to string
+var accountSid, authToken, from string
 var twilio *gotwilio.Twilio
 var last int64
 
@@ -91,22 +150,23 @@ func handler(w http.ResponseWriter, req *http.Request) {
 }
 
 func scrapeAndText() {
-    var timeSteelIdx uint = 3
-    fmt.Println("Starting scrape")
-    remaining := Scrape("597507018/pebble-time-awesome-smartwatch-no-compromises", timeSteelIdx)
-    if remaining < 0 {
-        fmt.Println("Failed scrape")
-        return
-    }
-    fmt.Println(remaining, "remaining")
-    if !sendMessage(last, remaining) {
-        return
-    }
+    // var timeSteelIdx uint = 3
+    // fmt.Println("Starting scrape")
+    // remaining, total, err := scrape("597507018/pebble-time-awesome-smartwatch-no-compromises", timeSteelIdx)
+    // if remaining < 0 {
+    //     fmt.Println("Failed scrape")
+    //     return
+    // }
+    // fmt.Println(remaining, "remaining")
+    // if !sendMessage(last, remaining) {
+    //     last = remaining
+    //     return
+    // }
 
-    last = remaining
-    message := fmt.Sprintf("%d Pebble Time Steels of %d are remaining.", remaining, 20000)
-    fmt.Printf("Sending message: %s\n", message)
-    twilio.SendSMS(from, to, message, "", "")
+    // last = remaining
+    // message := fmt.Sprintf("%d Pebble Time Steels of %d are remaining.", remaining, 20000)
+    // fmt.Printf("Sending message: %s\n", message)
+    // twilio.SendSMS(from, to, message, "", "")
 }
 
 func pingHandler(w http.ResponseWriter, req *http.Request) {
@@ -116,13 +176,14 @@ func pingHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-    accountSid = os.Getenv("twilioSid")
-    authToken = os.Getenv("twilioToken")
-    twilio = gotwilio.NewTwilioClient(accountSid, authToken)
-    from = os.Getenv("fromNum")
-    to = os.Getenv("toNum")
+    // twilioSid := os.Getenv("TWILIO_SID")
+    // twilioToken := os.Getenv("TWILIO_TOKEN")
+    // twilioClient := gotwilio.NewTwilioClient(accountSid, authToken)
+    // twilioNumber := os.Getenv("TWILIO_NUMBER")
+    // redisUrl := os.Getenv("REDISCLOUD_URL")
 
-    rgx = regexp.MustCompile("\\((.*?) ")
+    rewardsParser = &RewardsParser{}
+
     last = -1
 
     http.HandleFunc("/", handler)
--- main_test.go
+++ main_test.go
@@ -4,38 +4,65 @@ import (
     "testing"
 )
 
+func TestParseRewardAmountsNormal(te *testing.T) {
+    r, t, err := (&RewardsParser{}).parseRewardAmounts("Limited (5093 left of 20000)")
+    if err != nil {
+        te.Fail()
+    }
+    if r != 5093 || t != 20000 {
+        te.Fail()
+    }
+}
+
+func TestParseRewardAmountsZero(te *testing.T) {
+    r, _, err := (&RewardsParser{}).parseRewardAmounts("All gone!")
+    if err != nil {
+        te.Fail()
+    }
+    if r != 0 {
+        te.Fail()
+    }
+}
+
+func TestParseRewardAmountsFailure(te *testing.T) {
+    _, _, err := (&RewardsParser{}).parseRewardAmounts("Aseidjn2871(0")
+    if err == nil {
+        te.Fail()
+    }
+}
+
 func TestSendMessage1(t *testing.T) {
-    if send := sendMessage(550, 545); send {
+    if sendMessage(550, 545) {
         t.Fail()
     }
 }
 
 func TestSendMessage2(t *testing.T) {
-    if send := sendMessage(550, 500); !send {
+    if !sendMessage(550, 500) {
         t.Fail()
     }
 }
 
 func TestSendMessage3(t *testing.T) {
-    if send := sendMessage(1000, 999); send {
+    if sendMessage(1000, 999) {
         t.Fail()
     }
 }
 
 func TestSendMessage4(t *testing.T) {
-    if send := sendMessage(9, 8); !send {
+    if !sendMessage(9, 8) {
         t.Fail()
     }
 }
 
 func TestSendMessage5(t *testing.T) {
-    if send := sendMessage(0, 1); !send {
+    if !sendMessage(0, 1) {
         t.Fail()
     }
 }
 
 func TestSendMessage6(t *testing.T) {
-    if send := sendMessage(0, 0); send {
+    if sendMessage(0, 0) {
         t.Fail()
     }
 }
